17feb7e95bd3ce51baabd021c516b7f54f59fc1d
DMarrs <diegomarrs@gmail.com>
2022-10-26 15:57:45 -0700

    added form component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import Test from './components/Test.svelte'
+  import Form from './components/Form.svelte'
 </script>
 
 <Test text="hello there"/>
+<Form></Form>
 
 <style>
 
---
+++ src/components/Form.svelte
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<form>
+    <label for="term">Term</label>
+    <input id="term"/>
+
+    <label for="media">Media</label>
+    <select name="media" id="media">
+        <option value="movie">Movie</option>
+        <option value="podcast">Podcast</option>
+        <option value="music">Music</option>
+        <option value="tvShow">TV Show</option>
+    </select>
+
+    <button>Search</button>
+</form>
+
+<style>
+    form > * {
+        display: block;
+    }
+</style>
