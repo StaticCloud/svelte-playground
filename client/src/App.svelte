ae86c52d4c66e2355aef41b970f1f9167b6826b8
DMarrs <diegomarrs@gmail.com>
2022-11-02 12:33:04 -0700

    setup apollo client for svelte

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
+  import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+  import { setClient } from "svelte-apollo";
+
   import AudioPlayer from './components/AudioPlayer.svelte';
   import Movie from './components/Movie.svelte';
   import Test from './components/Test.svelte'
   import Form from './components/Form.svelte'
   let data = [];
+
+  const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache()
+  });
+
+  setClient(client);
 </script>
 
 <div>
